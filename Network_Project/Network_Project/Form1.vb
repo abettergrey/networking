9d81fb38fc7f83b460b9613e2adfc3e0051acb3a
Brett Yeager <abettergrey@gmail.com>
2014-11-16 18:58:45 -0500

    Created data base and cleared it out on load

--- Network_Project/Network_Project/Form1.vb
+++ Network_Project/Network_Project/Form1.vb
@@ -1,3 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Data.OleDb
+Imports System.Windows.Forms
+Public Class Form1
+    Dim strConn As String
+    Dim strDBName As String = "DB.mdb"
+    Dim DBCmd As OleDbCommand = New OleDbCommand
+    Dim DBConn As OleDbConnection
+    Sub send_move(tile_from As Integer, tile_to As Integer, myturn As String)
+        Dim strSQLCommand As String = "INSERT INTO checkers (id, turn, tile_number_from, tile_number_to) VALUES (NULL, " & myturn _
+                                      & ", " & tile_from & ", " & tile_to & ")"
 
+        strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & strDBName
+        DBConn = New OleDbConnection(strConn)
+        DBConn.Open()
+        DBCmd.CommandText = strSQLCommand
+        DBCmd.Connection = DBConn
+        DBCmd.ExecuteNonQuery()
+        DBConn.Close()
+    End Sub
 End Class
---
+++ Network_Project/Network_Project/Module1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+
+'Module1 is called on startup rather than the standard form.
+'This allows us to check the database before the form is loaded.
+Module Module1
+    Sub Main()
+        Dim strConn As String
+        Dim strDBName As String = "DB.mdb"
+        Dim myForm As New Form1
+
+        strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & strDBName
+
+        If Not File.Exists(strDBName) Then
+            MakeDataBase(strConn)
+        End If
+        'ClearDatabase(strConn)
+
+        myForm.ShowDialog()
+    End Sub
+
+    'Creates the database if it doesn't exist
+    Sub MakeDataBase(strConn As String)
+        Dim DBCat As New ADOX.Catalog
+        Dim DBConn As OleDbConnection
+        Dim DBCmd As OleDbCommand = New OleDbCommand
+
+        DBCat.Create(strConn)
+        DBConn = New OleDbConnection(strConn)
+
+        DBConn.Open()
+
+        DBCmd.CommandText = "CREATE TABLE checkers(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY( id ), turn VARCHAR(10), tile_number_from INT, tile_number_to INT)"
+        DBCmd.Connection = DBConn
+        DBCmd.ExecuteNonQuery()
+        DBConn.Close()
+
+    End Sub
+
+    'clears all data for a new game
+    Sub ClearDatabase(strConn As String)
+        Dim DBConn As OleDbConnection
+        Dim DBCmd As OleDbCommand = New OleDbCommand
+
+        DBConn = New OleDbConnection(strConn)
+        DBConn.Open()
+        DBCmd.Connection = DBConn
+        DBCmd.CommandText = "DELETE * FROM checkers"
+        DBCmd.ExecuteNonQuery()
+        DBConn.Close()
+    End Sub
+End Module
